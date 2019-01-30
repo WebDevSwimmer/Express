b9bb22defcffa679d6fbbfa1952b92454549495e
beginner <me@example.com>
2019-01-29 19:23:02 -0800

    component

--- index.js
+++ index.js
@@ -16,15 +16,27 @@ app.get('/first_template', function(req, res){
   res.render('first_view');
 });
 
-//Simple request time logger
-app.use(function(req, res, next){
-	console.log("A new request received at " + Date.now());
 
-	//This function call is very important. It tells that more processing is
-	//required for the current request and is in the next middleware
-	//function/route handler.
-	next();
+app.get('/dynamic_view', function(req, res){
+   res.render('dynamic', {
+      name: "TutorialsPoint", 
+      url:"http://www.tutorialspoint.com"
+   });
 });
+
+app.get('/components', function(req, res){
+  res.render('content');
+});
+
+//Simple request time logger
+	// app.use(function(req, res, next){
+	// 	console.log("A new request received at " + Date.now());
+
+	// 	//This function call is very important. It tells that more processing is
+	// 	//required for the current request and is in the next middleware
+	// 	//function/route handler.
+	// 	next();
+	// });
 
 app.get('/things/:name/:id', function(req, res) {
   res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
