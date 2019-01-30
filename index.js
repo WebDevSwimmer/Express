cd2844927e0093b8e9af6f0ca12dcbd391821681
beginner <me@example.com>
2019-01-29 19:11:06 -0800

    middleware

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ var app = express();
 //    res.send('The id you specified is ' + req.params.id);
 // });
 
+//Simple request time logger
+app.use(function(req, res, next){
+	console.log("A new request received at " + Date.now());
+
+	//This function call is very important. It tells that more processing is
+	//required for the current request and is in the next middleware
+	//function/route handler.
+	next();
+});
+
 app.get('/things/:name/:id', function(req, res) {
   res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
 });
